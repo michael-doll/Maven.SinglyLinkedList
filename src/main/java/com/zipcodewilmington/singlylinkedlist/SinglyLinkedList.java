336d5bfc22e328a09ab3a2b2e6d10866f93216c2
michael-doll <MWDoll88@gmail.com>
2019-11-17 20:00:51 -0500

    save work, add and contains methods

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -1,7 +1,82 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import javax.xml.soap.Node;
+import java.util.Iterator;
+import java.util.LinkedList;
+
 /**
  * Created by leon on 1/10/18.
  */
-public class SinglyLinkedList {
+public class SinglyLinkedList<E> implements Iterable {
+
+    class Node {
+        E data;
+        Node next;
+
+        public Node(E data) {
+        this.data = data;
+        this.next = null;
+        }
+    }
+
+    public Node head = null;
+    public Node tail = null;
+
+
+    public SinglyLinkedList(){
+        this.head = head;
+        this.tail = tail;
+    }
+
+//add remove contains find size get copy sort
+
+    public void add(E data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head = newNode;
+            tail = newNode;
+        }else{
+            tail.next = newNode;
+            tail = newNode;
+        }
+    }
+
+    public void remove(){}
+
+    public Boolean contains(E data){
+        Node current = head;
+
+        if(head == null){
+            return false;
+        }
+        while(current != null){
+            if(current.data.equals(data)){
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+
+    public Integer find(){return 0; }
+    public E copy(){return null; }
+    public E sort(){return null ; }
+
+    public Iterator iterator() { return null; }
+
+    //Testing
+//    public void display() {
+//        Node current = head;
+//
+//        if(head == null) {
+//            System.out.println("List is empty");
+//            return;
+//        }
+//        System.out.println("Nodes of singly linked list: ");
+//        while(current != null) {
+//            System.out.print(current.data + " ");
+//            current = current.next;
+//        }
+//        System.out.println();
+//    }
 }
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,7 +1,27 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedListTest {
+    @Test
+    public void addIntegerTest(){
+        SinglyLinkedList<Integer> singlyLinkedInt = new SinglyLinkedList<Integer>();
+        singlyLinkedInt.add(10);
+
+        Assert.assertTrue(singlyLinkedInt.contains(10));
+    }
+    @Test
+    public void addIntegerTest2(){
+        SinglyLinkedList<Integer> singlyLinkedInt = new SinglyLinkedList<Integer>();
+        singlyLinkedInt.add(10);
+
+        Assert.assertFalse(singlyLinkedInt.contains(20));
+        ;
+    }
+
 }
